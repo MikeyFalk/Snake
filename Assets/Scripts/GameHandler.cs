312f7f63f60bda9e0380a05ec894d8f7e5316133
Mike Falk <falk478@yahoo.com>
2021-03-10 20:41:18 -0800

    added movement and controls for the snake head in the game.

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour
 {
@@ -8,6 +10,19 @@ public class GameHandler : MonoBehaviour
     void Start()
     {
         Debug.Log("GameHandler.Start");
+
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+
+       /* int number = 0;
+        FunctionPeriodic.Create(() =>
+        {          
+            CMDebug.TextPopupMouse("Ding " + number);
+            number ++;
+        }, 
+        .3f);
+       */
     }
 
     // Update is called once per frame
